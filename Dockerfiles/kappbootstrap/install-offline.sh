cb28610b2e74f3e35f90d116049a8637feadd601
Adam Fowler <adam@adamfowler.org>
2022-04-08 19:35:24 +0100

    Install offline extra debugging
    Signed-off-by: Adam Fowler <adam@adamfowler.org>

--- Dockerfiles/kappbootstrap/install-offline.sh
+++ Dockerfiles/kappbootstrap/install-offline.sh
@@ -26,13 +26,26 @@ export YTT_registry__server=$INSTALL_REGISTRY_HOSTNAME
 export YTT_registry__username=$INSTALL_REGISTRY_USERNAME
 export YTT_registry__password=$INSTALL_REGISTRY_PASSWORD
 
+echo "## images.yaml before kapp:-"
+cat ./bundle/.imgpkg/images.yml
+
+
 ls -la .
 echo "## Deploying kapp-controller"
 ytt --debug -f ./bundle/kapp-controller/config/ -f ./bundle/registry-creds/ --data-values-env YTT --data-value-yaml kappController.deployment.concurrency=10 \
 	| kbld -f- -f ./bundle/.imgpkg/images.yml \
 	| kapp deploy --debug -a kapp-controller -n $ns_name -f- --yes
 
+echo "## images.yaml after first kapp:-"
+cat ./bundle/.imgpkg/images.yml
+
+echo "Sleeping so you can watch me..."
+sleep 10
+
 echo "## Deploying secretgen-controller"
 ytt --debug -f ./bundle/secretgen-controller/config/ -f ./bundle/registry-creds/ --data-values-env YTT \
 	| kbld -f- -f ./bundle/.imgpkg/images.yml \
 	| kapp deploy --debug -a secretgen-controller -n $ns_name -f- --yes
+
+echo "## images.yaml after second kapp:-"
+cat ./bundle/.imgpkg/images.yml
